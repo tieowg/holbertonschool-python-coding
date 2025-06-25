24167043dd9e5494c3f6104874c59b1ed079cf2a
tieowg <tieowg@example.com>
2025-06-25 14:00:43 +0000

    k

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Salam'''
+
+
+class Square:
+    '''Salam'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
