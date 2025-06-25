2903e8c5fd66f51e4966b25420f6b24b1d94d0ef
tieowg <tieowg@example.com>
2025-06-25 13:36:16 +0000

    k

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,3 +6,7 @@ class Square:
     '''Salam'''
     def __init__(self, size):
         self.__size = size
+        if not isinstance(size, int):
+            print("size must be an integer")
+        if size < 0:
+            print("size must be >= 0")
